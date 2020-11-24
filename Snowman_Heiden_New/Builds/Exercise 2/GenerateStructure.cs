b27691679e9f785d6e6bb619ea6870a2f60da097
jdh5563 <56982973+jdh5563@users.noreply.github.com>
2020-11-24 15:30:16 -0500

    Uploading Snowman Project

---
+++ Snowman_Heiden_New/Builds/Exercise 2/GenerateStructure.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateStructure : MonoBehaviour
+{
+	public GameObject structure;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//66, 1, 41 /// 47, 1, 27
+		Instantiate(structure, new Vector3(66, 1, 41), Quaternion.Euler(180, 0, 0));
+		Instantiate(structure, new Vector3(47, 1, 27), Quaternion.Euler(180, 0, 0));
+	}
+}
